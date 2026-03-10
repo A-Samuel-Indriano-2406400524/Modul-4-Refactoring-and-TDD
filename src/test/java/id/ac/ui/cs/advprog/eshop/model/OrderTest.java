fca60b7ca62dfc05021cc484fe017cb1ab92bc0e
samindriano <samuelindriano@gmail.com>
2026-03-10 09:14:42 +0700

    [RED] Add Order model skeleton.

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -74,7 +74,7 @@ public class OrderTest {
 
     @Test
     void testSetStatusToCancelled() {
-        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b", 
                 this.products, 1708560000L, "Safira Sudrajat");
         order.setStatus("CANCELLED");
         assertEquals("CANCELLED", order.getStatus());
